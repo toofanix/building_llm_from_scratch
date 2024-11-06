27597d118ee26b81ff7192917828cc53a93da077
jigarbandaria <jigarbandaria@gmail.com>
2024-11-06 11:17:01 -0500

    added new code for future use

--- utils.py
+++ utils.py
@@ -60,5 +60,120 @@ class MultiHeadAttention(nn.Module):
         # Add an optional linear projection
         context_vec = self.out_proj(context_vec)
         return context_vec
+    
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1+ torch.tanh(
+            torch.sqrt(torch.tensor(2.0/torch.pi)) *
+            (x + 0.44715 * torch.pow(x, 3))
+        ))
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4 * cfg['emb_dim']),
+            GELU(),
+            nn.Linear(4 * cfg['emb_dim'], cfg['emb_dim'])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+    
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg['emb_dim'],
+            d_out = cfg['emb_dim'],
+            context_length = cfg['context_length'],
+            num_heads = cfg['n_heads'],
+            dropout = cfg['drop_rate'],
+            qkv_bias = cfg['qkv_bias']
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg['emb_dim'])
+        self.norm2 = LayerNorm(cfg['emb_dim'])
+        self.drop_shortcut = nn.Dropout(cfg['drop_rate'])
+
+    def forward(self, x):
+        shortcut = x # assign `x` to shortcut to use for future steps
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x # assign `new x` to shortcut to use for future steps
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+# FINAL CLASS
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg['vocab_size'], cfg['emb_dim'])
+        self.pos_emb = nn.Embedding(cfg['context_length'], cfg['emb_dim'])
+        self.drop_emb = nn.Dropout(cfg['drop_rate'])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg['n_layers'])]
+        )
+
+        self.final_norm = LayerNorm(cfg['emb_dim'])
+        self.out_head = nn.Linear(cfg['emb_dim'], cfg['vocab_size'], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    """
+    To generate the next text one token at a time. Crop the context if the input 
+    is longer than context length.
+    idx is (batch, n_tokens)
+    """
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:] # pick the last context_size tokens
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :] # Take only the last time step as this is the next token
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=-1) # add the newly generated token to the original input
+
+    return idx            
 
 
